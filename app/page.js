f18e57e01d2ec5280a6b2d143ee7b985f5689541
Vaibhav Shivaji Bhosale <textmevaibhav@gmail.com>
2025-01-29 21:35:05 +0530

    intial commit

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
+    <>
    
+
+   <Spline scene="https://prod.spline.design/AIRJMCDBnJMKJTIe/scene.splinecode"  className="h-30 w-30"/>
+  
+
+    </>
   );
 }
